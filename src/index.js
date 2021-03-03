e4f950374d055149f26cb0e51f6432480a9ce322
Suhrid <suhrid@Suhrids-MacBook-Pro.local>
2021-03-03 19:13:33 +0530

    Homepage path added

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ app.use(express.json());
 app.use(userRoutes);
 app.use(taskRoutes);
 
+app.get("/",(req,res)=>{
+res.status(200).send("<h1>This is home page.</h1>")
+})
+
 app.listen(port, () => {
   console.log(`Server is listening to port: ${port}`);
 });
