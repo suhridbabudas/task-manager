95b4058d69fef7e28224d1c9c1fb23110fd2ef89
Suhrid <suhrid@Suhrids-MacBook-Pro.local>
2021-03-03 22:07:51 +0530

    Homepage path removed

--- src/index.js
+++ src/index.js
@@ -10,9 +10,6 @@ app.use(express.json());
 app.use(userRoutes);
 app.use(taskRoutes);
 
-app.get("/",(req,res)=>{
-res.status(200).send("<h1>This is home page.</h1>")
-})
 
 app.listen(port, () => {
   console.log(`Server is listening to port: ${port}`);
